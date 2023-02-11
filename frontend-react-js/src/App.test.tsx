0666b83d0272d33e09619a1bb90e358061598f0f
Marcus Holmgren <marcus.holmgren1@gmail.com>
2023-02-11 19:20:59 +0100

    chore: use vitest
    - Fix the only unit test

--- frontend-react-js/src/App.test.tsx
+++ frontend-react-js/src/App.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders landing page with menu', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Join The Party/i);
   expect(linkElement).toBeInTheDocument();
 });
--- frontend-react-js/vite.config.ts
+++ frontend-react-js/vite.config.ts
@@ -1,9 +1,21 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+    plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+    test: {
+      coverage: {
+        reporter: ['text', 'html'],
+        exclude: [
+          'node_modules/',
+          'src/setupTests.ts',
+        ],
+      },
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: './src/setupTests.ts',
+    },
 });
