fd108e24dbb671478b4bd409a51ec8f3e61fd977
Marcus Holmgren <marcus.holmgren1@gmail.com>
2023-02-25 15:15:51 +0100

    feat: Add /api/activities/notifications

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
+from flask_openapi3 import Info
+from flask_openapi3 import OpenAPI
 import os
 
 from services.home_activities import HomeActivities
+from services.notification_activities import NotificationActivites
 from services.user_activities import UserActivities
 from services.create_activity import CreateActivity
 from services.create_reply import CreateReply
@@ -13,7 +15,9 @@ from services.messages import Messages
 from services.create_message import CreateMessage
 from services.show_activity import ShowActivities
 
-app = Flask(__name__)
+info = Info(title="Cruddur API", version="0.0.1")
+app = OpenAPI(__name__, info=info)
+# app = Flask(__name__)
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
@@ -26,7 +30,7 @@ cors = CORS(
 )
 
 
-@app.route("/api/message_groups", methods=['GET'])
+@app.get("/api/message_groups")
 def data_message_groups():
     user_handle = 'andrewbrown'
     model = MessageGroups.run(user_handle=user_handle)
@@ -36,7 +40,7 @@ def data_message_groups():
         return model['data'], 200
 
 
-@app.route("/api/messages/@<string:handle>", methods=['GET'])
+@app.get("/api/messages/@<string:handle>")
 def data_messages(handle):
     user_sender_handle = 'andrewbrown'
     user_receiver_handle = request.args.get('user_reciever_handle')
@@ -50,7 +54,7 @@ def data_messages(handle):
     return
 
 
-@app.route("/api/messages", methods=['POST', 'OPTIONS'])
+@app.post("/api/messages", methods=['POST', 'OPTIONS'])
 @cross_origin()
 def data_create_message():
     user_sender_handle = 'andrewbrown'
@@ -66,13 +70,17 @@ def data_create_message():
     return
 
 
-@app.route("/api/activities/home", methods=['GET'])
+@app.get("/api/activities/home")
 def data_home():
     data = HomeActivities.run()
     return data, 200
 
+@app.get("/api/activities/notifications")
+def data_notifications():
+    data = NotificationActivites.run()
+    return data, 200
 
-@app.route("/api/activities/@<string:handle>", methods=['GET'])
+@app.get("/api/activities/@<string:handle>")
 def data_handle(handle):
     model = UserActivities.run(handle)
     if model['errors'] is not None:
@@ -81,7 +89,7 @@ def data_handle(handle):
         return model['data'], 200
 
 
-@app.route("/api/activities/search", methods=['GET'])
+@app.get("/api/activities/search")
 def data_search():
     term = request.args.get('term')
     model = SearchActivities.run(term)
@@ -92,7 +100,7 @@ def data_search():
     return
 
 
-@app.route("/api/activities", methods=['POST', 'OPTIONS'])
+@app.post("/api/activities", methods=['POST', 'OPTIONS'])
 @cross_origin()
 def data_activities():
     user_handle = 'andrewbrown'
@@ -106,13 +114,13 @@ def data_activities():
     return
 
 
-@app.route("/api/activities/<string:activity_uuid>", methods=['GET'])
+@app.get("/api/activities/<string:activity_uuid>")
 def data_show_activity(activity_uuid):
     data = ShowActivities.run(activity_uuid=activity_uuid)
     return data, 200
 
 
-@app.route("/api/activities/<string:activity_uuid>/reply", methods=['POST', 'OPTIONS'])
+@app.post("/api/activities/<string:activity_uuid>/reply", methods=['POST', 'OPTIONS'])
 @cross_origin()
 def data_activities_reply(activity_uuid):
     user_handle = 'andrewbrown'
--- frontend-react-js/src/App.tsx
+++ frontend-react-js/src/App.tsx
@@ -14,6 +14,7 @@ import {
   createBrowserRouter,
   RouterProvider
 } from "react-router-dom";
+import NotificationsFeedPage from "./pages/NotificationsFeedPage";
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
   {
     path: "/@:handle",
     element: <UserFeedPage />
+  },
+  {
+    path: "/notifications",
+    element: <NotificationsFeedPage />
   },
   {
     path: "/messages",
